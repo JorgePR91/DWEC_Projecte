a62c0bf5806eb430923182fa49584bbd4a4464f1
JorgePR91 <georgos14@gmail.com>
2025-10-02 10:18:37 +0200

    Primers passos

--- projecte-jorgepr/src/main.js
+++ projecte-jorgepr/src/main.js
@@ -1,17 +1,24 @@
 import "./style.scss";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+//import javascriptLogo from "./javascript.svg";
+//import viteLogo from "/vite.svg";
+//import { setupCounter } from "./counter.js";
 
 //import * as bootstrap from "bootstrap";
 //import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from 'bootstrap';  // opcional, si usas el JS de Bootstrap
 
-import { buildMenu } from "./components/header.js";
+import { buildMenu } from "./components/header";
+import { router } from "./router";
 
 document.addEventListener("DOMContentLoaded", async () => {
+    const appDiv = document.querySelector('#app');
   const menuDiv = document.querySelector("#menu");
   const containerDiv = document.querySelector("#container");
   containerDiv.innerHTML = "";
   menuDiv.append(buildMenu());
+
+    router(window.location.hash, appDiv);
+  window.addEventListener("hashchange", () => {
+    router(window.location.hash, appDiv);
+  });
 });
