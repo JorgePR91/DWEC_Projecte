995cd3ff9dbd7fbdddbd349fd26590ba752e6fd7
JorgePR91 <georgos14@gmail.com>
2025-10-09 20:32:35 +0200

    Moviment 2.0 + Poma segura

--- projecte-jorgepr/src/components/content.js
+++ projecte-jorgepr/src/components/content.js
@@ -9,13 +9,23 @@ export function inici(volum) {
   let posicioInicialY = Math.floor(canvas.length / 2);
 
   canvas[posicioInicialX][posicioInicialY].pos = 1;
+  canvas[posicioInicialX][posicioInicialY].estat = "serp";
   pintar({ x: posicioInicialX, y: posicioInicialY }, "serp");
   afegirPoma(canvas);
 
   //MOVIMENT DE LA SERP
   document.addEventListener("keydown", (event) => {
-    console.log("moviment");
     moviment(event, canvas);
+
+    console.log("Moviment");
+
+    for (let fila of canvas) {
+      for (let element of fila) {
+        if (element.estat === "serp") {
+          console.log(element);
+        }
+      }
+    }
   });
 
   //EN UN LISTENER ELS PARÀMETRES QUE PODEM AGAFAR ENS ELS PASSA EL NAVEGADOR, PEL QUE SOLES POT PASSAR EVENT, NO CANVAS, JA QUE EL NAVEGADOR NO EL TÉ NI ÉS PART DELS SEUS RECURSOS
@@ -40,83 +50,48 @@ function moviment(event, canvas) {
 
   for (let fila of canvas)
     for (let element of fila) if (element.pos === 1) cap = element;
-  console.log(`Cap: `);
-  console.log(cap);
+  //console.log(`Cap: `);
+  //console.log(cap);
 
   if (!cap) return;
 
+  let coordNoves;
+
   switch (event.key) {
     case "ArrowUp":
-      {
-        let coordNoves = { x: cap.x, y: cap.y - 1 };
-        if (comprovarLimit(coordNoves, canvas)) {
-          disminuir(canvas);
-        } else {
-          for (let fila of canvas)
-            for (let element of fila) if (element.pos > 0) element.pos++;
-
-          canvas[coordNoves.x][coordNoves.y].pos++;
-          pintar(coordNoves, "serp");
-
-          canvas[coordNoves.x][coordNoves.y].estat === "poma"
-            ? afegirPoma(canvas)
-            : disminuir(canvas);
-        }
-      }
+      coordNoves = { x: cap.x, y: cap.y - 1 };
       break;
     case "ArrowRight":
-      {
-        let coordNoves = { x: cap.x + 1, y: cap.y };
-
-        if (comprovarLimit(coordNoves, canvas)) {
-          disminuir(canvas);
-        } else {
-          for (let fila of canvas)
-            for (let element of fila) if (element.pos > 0) element.pos++;
-          canvas[coordNoves.x][coordNoves.y].pos++;
-          pintar(coordNoves, "serp");
-
-          canvas[coordNoves.x][coordNoves.y].estat === "poma"
-            ? afegirPoma(canvas)
-            : disminuir(canvas);
-        }
-      }
+      coordNoves = { x: cap.x + 1, y: cap.y };
       break;
     case "ArrowDown":
-      {
-        let coordNoves = { x: cap.x, y: cap.y + 1 };
-
-        if (comprovarLimit(coordNoves, canvas)) {
-          disminuir(canvas);
-        } else {
-          for (let fila of canvas)
-            for (let element of fila) if (element.pos > 0) element.pos++;
-          canvas[coordNoves.x][coordNoves.y].pos++;
-          pintar(coordNoves, "serp");
-
-          canvas[coordNoves.x][coordNoves.y].estat === "poma"
-            ? afegirPoma(canvas)
-            : disminuir(canvas);
-        }
-      }
+      coordNoves = { x: cap.x, y: cap.y + 1 };
       break;
     case "ArrowLeft":
-      {
-        let coordNoves = { x: cap.x - 1, y: cap.y };
-
-        if (comprovarLimit(coordNoves, canvas)) {
-          disminuir(canvas);
-        } else {
-          for (let fila of canvas)
-            for (let element of fila) if (element.pos > 0) element.pos++;
-          canvas[coordNoves.x][coordNoves.y].pos++;
-          pintar(coordNoves, "serp");
-          canvas[coordNoves.x][coordNoves.y].estat === "poma"
-            ? afegirPoma(canvas)
-            : disminuir(canvas);
-        }
-      }
+      coordNoves = { x: cap.x - 1, y: cap.y };
       break;
+    default:
+      return;
+  }
+
+  if (comprovarLimit(coordNoves, canvas) || canvas[coordNoves.x][coordNoves.y].estat === "serp") {
+    disminuir(canvas);
+    //   for (let fila of canvas)
+    // for (let element of fila) if (element.pos > 1) element.pos--;
+  } else {
+    for (let fila of canvas)
+      for (let element of fila) if (element.pos > 0) element.pos++;
+    canvas[coordNoves.x][coordNoves.y].pos++;
+    pintar(coordNoves, "serp");
+
+    if (canvas[coordNoves.x][coordNoves.y].estat === "poma") {
+      afegirPoma(canvas);
+      canvas[coordNoves.x][coordNoves.y].estat = "serp";
+      canvas[coordNoves.x][coordNoves.y].estat === "serp";
+    } else {
+      canvas[coordNoves.x][coordNoves.y].estat = "serp";
+      disminuir(canvas);
+    }
   }
 }
 
@@ -127,20 +102,18 @@ function comprovarLimit(pos, canvas) {
 }
 
 function disminuir(canvas) {
-  //let cua;
-  // for (let fila of canvas)
-  //   for (let element of fila) if (element.pos > 0) element.pos++;
 
   let cua = canvas
     .flat()
     .reduce((accumulador, actual) =>
       actual.pos >= accumulador.pos ? actual : accumulador
     );
-    
-  cua.pos = 0;
-  console.log("Cua:");
-  console.log(cua);
 
+  //console.log("Cua:");
+  //console.log(cua);
+
+  cua.pos = 0;
+  cua.estat = null;
   borrar(cua, "serp");
 }
 
@@ -148,31 +121,40 @@ function disminuir(canvas) {
 function afegirPoma(canvas) {
   console.log("Afegint poma...");
 
-  let x = Math.floor(Math.random() * canvas.length);
-  let y = Math.floor(Math.random() * canvas.length);
+  //  let x = Math.floor(Math.random() * canvas.length);
+  //  let y = Math.floor(Math.random() * canvas.length);
+  
+  let arrCasLliures = canvas.flat().filter(c => c.estat === null);
+
+  if(arrCasLliures.length === 0)
+    finalitzarJoc();
+
+  let novaPoma = arrCasLliures[Math.floor(Math.random() * arrCasLliures.length)]
 
   let poma;
-  
-    for (let fila of canvas)
-      for (let element of fila) if (element.estat === "poma") poma =  element;
-    console.log('poma');
+
+  for (let fila of canvas)
+    for (let element of fila) if (element.estat === "poma") poma = element;
+  console.log("poma");
 
   console.log(poma);
 
   if (poma) {
-    borrar(poma, "poma");
     poma.estat = null;
-    console.log(poma)
+    borrar(poma, "poma");
   }
 
-  if (canvas[x][y].estat === null) {
-    canvas[x][y].estat = "poma";
-    pintar({ x: x, y: y }, "poma");
-  } else if (canvas.flat().filter((e) => e.estat === null).length == 0) {
-    finalitzarJoc();
-  } else {
-    afegirPoma(canvas);
-  }
+  // if (canvas[x][y].estat === null) {
+  //   canvas[x][y].estat = "poma";
+  //   pintar({ x: x, y: y }, "poma");
+  // } else if (canvas.flat().filter((e) => e.estat === null).length == 0) {
+  //   finalitzarJoc();
+  // } else {
+  //   afegirPoma(canvas);
+  // }
+
+    canvas[novaPoma.x][novaPoma.y].estat = "poma";
+    pintar(canvas[novaPoma.x][novaPoma.y], "poma");
 }
 
 export function renderContent(volum) {
@@ -225,20 +207,17 @@ export function renderCanvas(volum = 30) {
 }
 
 function pintar(coord, forma) {
-  let id = "x" + coord.x +"y"+ coord.y;
-
+  let id = "x" + coord.x + "y" + coord.y;
   let casella = document.getElementById(id);
+
   console.log("A pintar " + "#" + id + " forma: " + forma);
 
   casella.classList.add(forma);
   console.log(casella);
-
-  casella.estat = forma;
 }
 
 function borrar(coord, forma) {
-  let id = "x" + coord.x +"y"+ coord.y;
-
+  let id = "x" + coord.x + "y" + coord.y;
   let casella = document.getElementById(id);
 
   console.log("A borrar " + "#" + id + " forma: " + forma);
@@ -246,9 +225,6 @@ function borrar(coord, forma) {
   casella.classList.remove(forma);
 
   console.log(casella);
-  
-  if(typeof coord == 'object')
-  coord.estat = null;
 }
 
 //FUNCIÓ DE FINALITZAR JOC
